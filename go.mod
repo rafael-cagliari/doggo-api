b727f45e8c4fac9308fd998523c7538bd5e69521
rafael-cagliari <rafael.cagliari22@gmail.com>
2025-02-09 10:17:08 -0300

    initial files and test routes implementation

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/rafael-cagliari/doggo-api
+
+go 1.22.4
+
+require github.com/go-chi/chi/v5 v5.2.1
+
+require (
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
+
+require github.com/sirupsen/logrus v1.9.3
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
+)
+
+type Dog struct {
+	Id    string `json:"id"`
+	Breed string `json:"breed"`
+	Name  string `json:"name"`
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			next.ServeHTTP(w, r)
+
+			duration := time.Since(start)
+
+			logger.Infof("Method: %s | Route: %s | IP: %s | Time: %s",
+				r.Method, r.URL.Path, r.RemoteAddr, duration)
+		})
+	})
+
+	router.Use()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API is running!"))
+	})
+
+	router.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API is running test!"))
+	})
+
+	router.Get("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		response := map[string]string{
+			"message": "Usuário encontrado!",
+			"userID":  id,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	router.Post("/users", func(w http.ResponseWriter, r *http.Request) {
+		var dog Dog
+
+		err := json.NewDecoder(r.Body).Decode(&dog)
+		if err != nil {
+			http.Error(w, "JSON parsing error", http.StatusBadRequest)
+			return
+		}
+		if dog.Breed == "persa" || dog.Breed == "sphynx" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": fmt.Sprintf("%s is not a dog breed", dog.Breed),
+			})
+			return
+		}
+
+		response := map[string]string{
+			"message": "Usuário cadastrado com sucesso!",
+			"id":      dog.Id,
+			"name":    dog.Name,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe("0.0.0.0:8080", router)
+
+}
